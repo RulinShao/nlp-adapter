cee2305b4dd631e9d375154ecf540e1d9b8ec1f3
RulinShao <Srl0310@outlook.com>
2021-05-25 17:15:21 +0800

    add avg_bwt test

--- src/args.py
+++ src/args.py
@@ -13,7 +13,7 @@ def parse_arguments():
     parser.add_argument(
         "--config", type=str, default=None, help="Config file to use, YAML format"
     )
-    parser.add_argument("--name", type=str, default="in21k_100_adapter_multi5_hard", help="Experiment id.")
+    parser.add_argument("--name", type=str, default="in21k_100_5adapter_hard", help="Experiment id.")
     parser.add_argument(
         "--log-dir",
         type=str,
